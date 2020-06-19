5d83a035dac58cdbebb433c93806b78eb1635c8f
jimmeryn <jimmeryn@gmail.com>
2020-06-19 16:33:32 +0200

    Project Structure

--- src/components/leftMenu/GroupComponent.tsx
+++ src/components/leftMenu/GroupComponent.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import { connect } from "react-redux";
 
-import { IData } from "../store/data/types";
+import { IData } from "../../store/data/types";
 
-class GroupComponent {
+export default class GroupComponent {
   render() {
     return <div></div>;
   }
--- src/components/leftMenu/LeftMenu.tsx
+++ src/components/leftMenu/LeftMenu.tsx
@@ -1,18 +1,14 @@
 import * as React from "react";
 import { useSelector } from "react-redux";
-import { LeftMenuState } from "../store";
-import GroupComponent from "../components/GroupComponent";
+import { LeftMenuState } from "../../store";
+import GroupComponent from "./GroupComponent";
 
 const LeftMenu: React.FunctionComponent = () => {
   const dataElement = useSelector(
     (state: LeftMenuState) => state.reducer.aquariums
   );
 
-  return (
-    <div className="NavBar-left">
-      <GroupComponent dataElement={dataElement} />
-    </div>
-  );
+  return <div className="NavBar-left">{/* <GroupComponent /> */}</div>;
 };
 
 export default LeftMenu;
