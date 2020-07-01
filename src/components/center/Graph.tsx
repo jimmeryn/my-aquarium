b93afbe375e8dcf289f788b98dc4e89a6d0f6be3
jimmeryn <jimmeryn@gmail.com>
2020-07-01 13:45:26 +0200

    second axis added

--- src/components/center/Graph.tsx
+++ src/components/center/Graph.tsx
@@ -18,16 +18,43 @@ const data = {
   ],
   datasets: [
     {
-      label: "Temperature",
-      data: [22, 19, 27, 23, 22, 24, 17, 25, 23, 24, 20, 19],
+      label: "Cl2",
+      yAxisID: "y-axis-1",
+      data: [31, 21, 123, 123, 12, 65, 21, 12, 32, 41, 21, 54],
       fill: false, // Don't fill area under the line
       borderColor: "lightblue" // Line color
+    },
+    {
+      label: "N03",
+      yAxisID: "y-axis-2",
+      data: [22, 19, 27, 23, 22, 24, 17, 25, 23, 24, 20, 19],
+      fill: false, // Don't fill area under the line
+      borderColor: "green" // Line color
     }
   ]
 };
 
 const options = {
-  maintainAspectRatio: false // Don't maintain w/h ratio
+  maintainAspectRatio: false, // Don't maintain width/height ratio
+  layout: { padding: 20 },
+  fullWidth: false,
+  scales: {
+    yAxes: [
+      {
+        id: "y-axis-1",
+        type: "linear",
+        position: "left"
+      },
+      {
+        id: "y-axis-2",
+        type: "linear",
+        position: "right",
+        gridLines: {
+          display: false
+        }
+      }
+    ]
+  }
 };
 
 const Graph: React.FunctionComponent = () => (
