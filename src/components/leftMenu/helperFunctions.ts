089e039d74093715f8d9fd33289e395158be9206
jimmeryn <jimmeryn@gmail.com>
2020-07-04 20:08:26 +0200

    getting latest data (params/refill)

---
+++ src/components/leftMenu/helperFunctions.ts
@@ -0,0 +1,19 @@
+import { Param, Refill } from "../../store/types";
+
+export const getLatestParams = (params: Param[]): Param[] => {
+  const latestParamDate = new Date(
+    Math.max.apply(null, params.map(e => new Date(e.date)))
+  );
+  return params.filter(
+    e => e.date.toISOString() === latestParamDate.toISOString()
+  );
+};
+
+export const getLatestRefill = (refills: Refill[]) => {
+  const latestRefillDate = refills
+    .map(e => e.date)
+    .sort()
+    .reverse()[0];
+
+  return refills.filter(e => e.date === latestRefillDate)[0];
+};
