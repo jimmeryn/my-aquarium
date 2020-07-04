a3d07e2fa40437a9ea2fb2873fc15bc51b218970
jimmeryn <jimmeryn@gmail.com>
2020-07-04 20:07:07 +0200

    if no refill provided show "No refills"

--- src/components/leftMenu/RefillComp.tsx
+++ src/components/leftMenu/RefillComp.tsx
@@ -2,8 +2,13 @@ import * as React from "react";
 import ListItem from "@material-ui/core/ListItem";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import { Refill } from "src/store/types";
 
-const Refill: React.FunctionComponent = () => (
+type RefillProps = {
+  latestRefill: Refill | undefined;
+};
+
+const RefillComp: React.FunctionComponent<RefillProps> = ({ latestRefill }) => (
   <div className="refill">
     <ListItem button>
       <Grid container alignItems="center">
@@ -11,11 +16,13 @@ const Refill: React.FunctionComponent = () => (
           <Typography gutterBottom>Refill</Typography>
         </Grid>
         <Grid item>
-          <Typography gutterBottom>10 [l]</Typography>
+          <Typography gutterBottom>
+            {latestRefill ? `${latestRefill.value} [l]` : "No refills"}
+          </Typography>
         </Grid>
       </Grid>
     </ListItem>
   </div>
 );
 
-export default Refill;
+export default RefillComp;
