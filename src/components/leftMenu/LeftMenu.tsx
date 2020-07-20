0b45529a11bc9318ee8b41db3a8c26ca78b6723f
jimmeryn <jimmeryn@gmail.com>
2020-07-20 22:15:19 +0200

    Added Title and page state to LeftMenu

--- src/components/leftMenu/LeftMenu.tsx
+++ src/components/leftMenu/LeftMenu.tsx
@@ -1,8 +1,12 @@
 import * as React from "react";
 import GroupComponentList from "./GroupComponentList";
+import Title from "./Title";
 
-const LeftMenu: React.FunctionComponent = () => (
+const LeftMenu: React.FunctionComponent<{ isLandingPageOpen: boolean }> = ({
+  isLandingPageOpen
+}) => (
   <div className="left-menu">
+    <Title />
     <GroupComponentList />
   </div>
 );
---
+++ src/components/leftMenu/Title.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Typography } from "@material-ui/core";
+
+const Title: React.FunctionComponent = () => (
+  <div className="title">
+    <Typography>My Aquarium</Typography>
+  </div>
+);
+
+export default Title;
