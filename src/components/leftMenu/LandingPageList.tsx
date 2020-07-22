a400257d12f21ea12b180fcc3ef5ed57317f987d
jimmeryn <jimmeryn@gmail.com>
2020-07-22 12:35:09 +0200

    Created menu when landing page is open

--- src/components/leftMenu/LandingPageList.tsx
+++ src/components/leftMenu/LandingPageList.tsx
@@ -1,7 +1,14 @@
 import * as React from "react";
+import AquariumOverviewComponent from "./AquariumOverviewComponent";
 
 const LandingPageList: React.FunctionComponent<{
   allaquariumsIds: number[];
-}> = ({ allaquariumsIds }) => <div></div>;
+}> = ({ allaquariumsIds }) => (
+  <div>
+    {allaquariumsIds.map(i => (
+      <AquariumOverviewComponent index={i} key={i} />
+    ))}
+  </div>
+);
 
 export default LandingPageList;
