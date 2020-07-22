a65b5293a813202d21a9793875f804998bc5c752
jimmeryn <jimmeryn@gmail.com>
2020-07-22 11:12:58 +0200

    Left Menu renders current menu state

--- src/components/leftMenu/LeftMenu.tsx
+++ src/components/leftMenu/LeftMenu.tsx
@@ -1,15 +1,27 @@
 import * as React from "react";
-import GroupComponentList from "./GroupComponentList";
 import Title from "./Title";
+import AquariumViewList from "./AquariumViewList";
+import LandingPageList from "./LandingPageList";
+import { useSelector } from "react-redux";
+import { State } from "src/store";
 
 const LeftMenu: React.FunctionComponent<{
   isLandingPageOpen: boolean;
   changeLandingPageState: React.Dispatch<React.SetStateAction<boolean>>;
-}> = ({ isLandingPageOpen, changeLandingPageState }) => (
-  <div className="left-menu">
-    <Title changeLandingPageState={changeLandingPageState} />
-    <GroupComponentList isLandingPageOpen={isLandingPageOpen} />
-  </div>
-);
+}> = ({ isLandingPageOpen, changeLandingPageState }) => {
+  const { allaquariumsIds, aquariumsById } = useSelector(
+    (state: State) => state.aquariums
+  );
+  return (
+    <div className="left-menu">
+      <Title changeLandingPageState={changeLandingPageState} />
+      {isLandingPageOpen ? (
+        <LandingPageList allaquariumsIds={allaquariumsIds} />
+      ) : (
+        <AquariumViewList aquarium={aquariumsById[0]} />
+      )}
+    </div>
+  );
+};
 
 export default LeftMenu;
