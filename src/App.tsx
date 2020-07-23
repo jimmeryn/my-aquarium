00f0d8ed4998a31e41c0bd1df9e1428e36227fd8
jimmeryn <jimmeryn@gmail.com>
2020-07-23 20:41:34 +0200

    containers dispatching and using state

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,14 @@ import "./styles/menu.css";
 import "./styles/center.css";
 import "./styles/nav.css";
 
-import LeftMenu from "./components/leftMenu/LeftMenu";
-import TopNav from "./components/topNav/TopNav";
-import Center from "./components/center/Center";
-import { useSelector } from "react-redux";
-import { State } from "./store";
+import Center from "./containers/Center";
 
 const App: React.FunctionComponent = () => {
-  const { visibleAquarium, allaquariumsIds, aquariumsById } = useSelector(
-    (state: State) => ({
-      visibleAquarium: state.visibleAquarium,
-      allaquariumsIds: state.aquariums.allaquariumsIds,
-      aquariumsById: state.aquariums.aquariumsById
-    })
-  );
-
   return (
     <div className="App">
-      <LeftMenu
-        visibleAquarium={visibleAquarium}
-        allaquariumsIds={allaquariumsIds}
-        aquariumsById={aquariumsById}
-      />
+      <LeftMenu />
       <div className="app-container">
-        <TopNav />
-        <Center visibleAquarium={visibleAquarium} />
+        <Center />
       </div>
     </div>
   );
