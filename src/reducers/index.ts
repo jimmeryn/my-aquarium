6445f3f5e853cb0c3f9aee861cab75b3eb0a3169
jimmeryn <jimmeryn@gmail.com>
2020-07-26 15:41:58 +0200

    Combined with param filter reducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
 import aquariums from "./aquariums";
 import visibleAquarium from "./visibleAquarium";
+import paramFilter from "./paramFilter";
 
-const reducer = combineReducers({ aquariums, visibleAquarium });
+const reducer = combineReducers({ aquariums, visibleAquarium, paramFilter });
 
 export default reducer;
