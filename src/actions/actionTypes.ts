0569e8d30dbde4022ed81f5c5ab826048c15ee66
jimmeryn <jimmeryn@gmail.com>
2020-08-03 11:43:51 +0200

    graph actions

--- src/actions/actionTypes.ts
+++ src/actions/actionTypes.ts
@@ -5,6 +5,8 @@ export const ADD_AQUARIUM = "ADD_AQUARIUM";
 export const ADD_AQUARIUM_DATA = "ADD_AQUARIUM_DATA";
 export const SET_VISIBLE_AQUARIUM = "SET_VISIBLE_AQUARIUM";
 export const CHANGE_PARAM_FILTER = "CHANGE_PARAM_FILTER";
+export const ADD_GRAPH_DATA = "ADD_GRAPH_DATA";
+export const SHOW_ON_GRAPH = "SHOW_ON_GRAPH";
 
 interface AddAquarium {
   readonly type: typeof ADD_AQUARIUM;
@@ -32,6 +34,22 @@ interface ChagneParamFilter {
   readonly type: typeof CHANGE_PARAM_FILTER;
   filter: string;
 }
+
+interface AddGraphData {
+  readonly type: typeof ADD_GRAPH_DATA;
+  label: string;
+  data: number[];
+  dates: string[];
+}
+
+interface ShowGraphData {
+  readonly type: typeof SHOW_ON_GRAPH;
+  label: string;
+  data: number[];
+  dates: string[];
+}
+
+export type GraphActionTypes = AddGraphData | ShowGraphData;
 
 export type ActionTypes =
   | AddAquarium
