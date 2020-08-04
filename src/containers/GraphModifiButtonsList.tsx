fb373338ef33bfc9af698d30016470e67619df24
jimmeryn <jimmeryn@gmail.com>
2020-08-04 13:26:02 +0200

    graph modify button list

---
+++ src/containers/GraphModifiButtonsList.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import GraphModifyButton from "../components/GraphModifButton";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import { GraphActionTypes, ADD_GRAPH_DATA, SHOW_ON_GRAPH } from "../actions";
+import { State } from "../store";
+
+const GraphModifiButtonsList: React.FunctionComponent = () => {
+  const { paramFilter, labels } = useSelector((state: State) => ({
+    aquariumsById: state.aquariums.aquariumsById,
+    visibleAquarium: state.visibleAquarium,
+    paramFilter: state.paramFilter,
+    labels: state.graphLabels
+  }));
+
+  const dispatch = useDispatch<React.Dispatch<GraphActionTypes>>();
+  const graphDispatch = (
+    type: typeof ADD_GRAPH_DATA | typeof SHOW_ON_GRAPH
+  ) => () => dispatch({ type, label: paramFilter });
+
+  return (
+    <div className="graph-modify-list">
+      <BottomNavigation value={10}>
+        <GraphModifyButton
+          label={"Show"}
+          onClick={graphDispatch(SHOW_ON_GRAPH)}
+        />
+        <GraphModifyButton
+          label={"Add"}
+          onClick={graphDispatch(ADD_GRAPH_DATA)}
+        />
+      </BottomNavigation>
+    </div>
+  );
+};
+
+export default GraphModifiButtonsList;
