f4a2331de1184e0394311d8225df5e17f3728267
jimmeryn <jimmeryn@gmail.com>
2020-08-05 14:13:20 +0200

    classnames

--- src/containers/Menu.tsx
+++ src/containers/Menu.tsx
@@ -4,17 +4,17 @@ import LandingPageMenu from "./LandingPageMenu";
 import { useSelector } from "react-redux";
 import { State } from "../store";
 
+const classNames = require("classnames");
+
 const Menu: React.FunctionComponent = () => {
   const { visibleAquarium, allaquariumsIds, aquariumsById } = useSelector(
     (state: State) => ({
       visibleAquarium: state.visibleAquarium,
       allaquariumsIds: state.aquariums.allaquariumsIds,
-      aquariumsById: state.aquariums.aquariumsById
-    })
-  );
+  const menuClass = classNames({ menu: !menuState, "menu--active": menuState });
 
   return (
-    <div className="menu">
+    <div className={menuClass} ref={wrapperRef}>
       {visibleAquarium === -1 ? (
         <LandingPageMenu allaquariumsIds={allaquariumsIds} />
       ) : (
