ec539c14323acfcf02bd3a4339f12f5e0134dc8f
jimmeryn <jimmeryn@gmail.com>
2020-08-05 14:18:38 +0200

    click outside of menu handle - mobile

--- src/containers/Menu.tsx
+++ src/containers/Menu.tsx
@@ -6,11 +6,32 @@ import { State } from "../store";
 
 const classNames = require("classnames");
 
+function useOutsideClick(
+  ref: React.MutableRefObject<any>,
+  menuState: boolean,
+  setMenuState: () => void
+) {
+  React.useEffect(() => {
+    const handleClickOutside = event => {
+      if (ref.current && !ref.current.contains(event.target) && menuState) {
+        setMenuState();
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref, menuState]);
+}
+
 const Menu: React.FunctionComponent = () => {
   const { visibleAquarium, allaquariumsIds, aquariumsById } = useSelector(
     (state: State) => ({
       visibleAquarium: state.visibleAquarium,
       allaquariumsIds: state.aquariums.allaquariumsIds,
+  const wrapperRef = React.useRef(null);
+  useOutsideClick(wrapperRef, menuState, setMenuStateDispatch);
+
   const menuClass = classNames({ menu: !menuState, "menu--active": menuState });
 
   return (
