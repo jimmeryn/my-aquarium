f83db68dd1f9a1904039d932b25b2528c15b67db
jimmeryn <jimmeryn@gmail.com>
2020-08-07 13:43:10 +0200

    dark theme - color base on background

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,9 +1,20 @@
 import * as React from "react";
 
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
 import CloseButton from "./CloseButton";
+const theme = createMuiTheme({
+  palette: {
+    type: "dark"
+  }
+});
+
 const Dialog: React.FunctionComponent<{
   isDialogOpen: boolean;
         <CloseButton />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
 );
 
 export default Dialog;
