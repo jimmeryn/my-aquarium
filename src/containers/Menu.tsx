1dc62d3caaef58d0be727bdbfe67a3dbb2a09fd0
jimmeryn <jimmeryn@gmail.com>
2020-08-12 12:46:54 +0200

    deleted outside click - mobile bug

--- src/containers/Menu.tsx
+++ src/containers/Menu.tsx
@@ -1,32 +1,13 @@
 import * as React from "react";
 
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { State } from "../store";
-import { UIActionTypes, SET_MENU_STATE } from "../actions";
 
 import AquariumViewMenu from "./AquariumViewMenu";
 import LandingPageMenu from "./LandingPageMenu";
 import Dialog from "./Dialog";
 
 const classNames = require("classnames");
-
-function useOutsideClick(
-  ref: React.MutableRefObject<any>,
-  menuState: boolean,
-  setMenuState: () => void
-) {
-  React.useEffect(() => {
-    const handleClickOutside = event => {
-      if (ref.current && !ref.current.contains(event.target) && menuState) {
-        setMenuState();
-      }
-    };
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [ref, menuState]);
-}
 
 const Menu: React.FunctionComponent = () => {
   const {
@@ -40,17 +21,10 @@ const Menu: React.FunctionComponent = () => {
     aquariumsById: state.aquariums.aquariumsById,
     menuState: state.userInterface.menu
   }));
-
-  const dispatchMenu = useDispatch<React.Dispatch<UIActionTypes>>();
-  const setMenuStateDispatch = () => dispatchMenu({ type: SET_MENU_STATE });
-
-  const wrapperRef = React.useRef(null);
-  useOutsideClick(wrapperRef, menuState, setMenuStateDispatch);
-
   const menuClass = classNames({ menu: !menuState, "menu--active": menuState });
 
   return (
-    <div className={menuClass} ref={wrapperRef}>
+    <div className={menuClass}>
       {visibleAquarium === -1 ? (
         <LandingPageMenu allaquariumsIds={allaquariumsIds} />
       ) : (
