8dc5956b0f94bee56f969a9af79ff786e469fdcc
jimmeryn <jimmeryn@gmail.com>
2020-08-15 18:51:32 +0200

    using extracted components

--- src/components/DialogComponent.tsx
+++ src/components/DialogComponent.tsx
@@ -1,35 +1,38 @@
 import * as React from "react";
 
-import DateFnsUtils from "@date-io/date-fns";
-import {
-  MuiPickersUtilsProvider,
-  KeyboardDatePicker
-} from "@material-ui/pickers";
-import { CssBaseline, InputAdornment, TextField } from "@material-ui/core";
 import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
 
 import Divider from "./Divider";
 import GroupButton from "./GroupButton";
 import CloseButton from "./CloseButton";
+import DatePicker from "./DatePicker";
+import ParamTableInput from "./ParamTableInput";
+import TextInput from "./TextInput";
 
-const theme = createMuiTheme({
+const darkTheme = createMuiTheme({
   palette: {
     type: "dark"
   }
 });
 
 const DialogComponent: React.FunctionComponent<{
-  selectedDate: Date;
-  setSelectedDate: React.Dispatch<React.SetStateAction<Date>>;
   title: string;
-  unit: string;
   onClose: any;
-}> = ({ selectedDate, setSelectedDate, title, unit, onClose }) => {
-  const handleDateChange = (date: Date | null) => {
-    setSelectedDate(date);
-  };
-
+  label?: string;
+  unit?: string;
+  table?: boolean;
+  selectedDate?: Date;
+  setSelectedDate?: React.Dispatch<React.SetStateAction<Date>>;
+}> = ({
+  title,
+  onClose,
+  label,
+  unit,
+  table,
+  selectedDate,
+  setSelectedDate
+}) => {
   return (
     <div className="dialog">
       <div className="dialog-title-wrappper">
@@ -37,33 +40,15 @@ const DialogComponent: React.FunctionComponent<{
         <div className="dialog-title">{title}</div>
         <Divider />
       </div>
-
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <TextField
-          label="Value"
-          id="standard-start-adornment"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="start">{`[${unit}]`}</InputAdornment>
-            )
-          }}
-        />
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <KeyboardDatePicker
-            disableToolbar
-            variant="inline"
-            format="dd/MM/yyyy"
-            margin="normal"
-            id="date-picker-inline"
-            label="Date"
-            value={selectedDate}
-            onChange={handleDateChange}
-            KeyboardButtonProps={{
-              "aria-label": "change date"
-            }}
+      <ThemeProvider theme={darkTheme}>
+        {label ? <TextInput label={label} unit={unit} /> : null}
+        {table ? <ParamTableInput /> : null}
+        {selectedDate && setSelectedDate ? (
+          <DatePicker
+            selectedDate={selectedDate}
+            setSelectedDate={setSelectedDate}
           />
-        </MuiPickersUtilsProvider>
+        ) : null}
       </ThemeProvider>
       <GroupButton
         className={"dialog-button"}
