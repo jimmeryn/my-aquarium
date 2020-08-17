0d5692579bdd76fe63e5ce4abcb620fb65a175f0
jimmeryn <jimmeryn@gmail.com>
2020-08-17 11:39:10 +0200

    return one component with predefined props

--- src/containers/Dialog.tsx
+++ src/containers/Dialog.tsx
@@ -19,49 +19,46 @@ const Dialog: React.FunctionComponent<{
   const { aquariumsById } = useSelector((state: State) => state.aquariums);
   const paramList = [...new Set([...aquariumsById[0].params.map(e => e.name)])];
 
-  // TODO: DRY - return one dialog component but with different params based on variant
-  // if always returning component than we need to add classname --hidden back.
-  // Currently just returning null, so no dialog is visible
-  return (() => {
-    switch (variant) {
-      case REFILL:
-        return (
-          <DialogComponent
-            title={"New Water Refill"}
-            onClose={() => setDialogState(HIDDEN)}
-            label={"Value"}
-            unit={"l"}
-            selectedDate={selectedDate}
-            setSelectedDate={setSelectedDate}
-          />
-        );
-      case PARAMS:
-        return (
-          <DialogComponent
-            title={"New Parameters"}
-            onClose={() => setDialogState(HIDDEN)}
-            paramNames={paramList}
-            selectedDate={selectedDate}
-            setSelectedDate={setSelectedDate}
-          />
-        );
-      case AQUARIUM:
-        return (
-          <DialogComponent
-            title={"New Aquarium"}
-            onClose={() => setDialogState(HIDDEN)}
-            selectedDate={selectedDate}
-            setSelectedDate={setSelectedDate}
-            label={"Aquarium Size"}
-            unit={"l"}
-            dateLabel={"Start Date"}
-          />
-        );
+  const dialogData = {
+    title: "",
+    label: "",
+    unit: "",
+    paramNames: null,
+    dateLabel: ""
+  };
 
-      default:
-        return null;
-    }
-  })();
+  switch (variant) {
+    case PARAMS:
+      dialogData.title = "Parameters";
+      dialogData.paramNames = paramList;
+      break;
+    case REFILL:
+      dialogData.title = "Water Refill";
+      dialogData.label = "Value";
+      dialogData.unit = "l";
+      break;
+    case AQUARIUM:
+      dialogData.title = "Aquarium";
+      dialogData.label = "Aquarium Size";
+      dialogData.unit = "l";
+      dialogData.dateLabel = "Start Date";
+      break;
+    default:
+      break;
+  }
+
+  return variant !== HIDDEN ? (
+    <DialogComponent
+      title={dialogData.title}
+      label={dialogData.label}
+      unit={dialogData.unit}
+      paramNames={dialogData.paramNames}
+      dateLabel={dialogData.dateLabel}
+      selectedDate={selectedDate}
+      onClose={() => setDialogState(HIDDEN)}
+      setSelectedDate={setSelectedDate}
+    />
+  ) : null;
 };
 
 export default Dialog;
